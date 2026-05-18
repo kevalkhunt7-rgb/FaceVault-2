f5cec38f5c0e59e578aff69c3c42ed9363af899a
keval khunt <kevalkhunt7@gmail.com>
2026-05-18 18:16:05 +0530

    final working

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://facevault-2.onrender.com';
 export default API_URL;
